223044eaa924883e5149dc3bf4b51c3bc1c02184
Unknown <riderjensen@gmail.com>
2018-04-11 13:12:02 -0600

    Adding a minor get

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("hello world"); 
+});
+      
+
+    app.listen(process.env.PORT, ()=> console.log('App is running on ' + process.env.port));
