8058f01010a7ba0cee395fa97968f71137293012
Unknown <riderjensen@gmail.com>
2018-04-13 16:57:58 -0600

    Adding styles, chat route not working, styles not connected

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 
-
 const port = process.env.PORT;
 const app = express();
 
 // routes
 const chatRouter = require('./src/routes/chatRouter');
 
+app.use(express.static('/public'));
 app.use('/chat', chatRouter);
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-    res.send('index page');
+    res.render('index', {
+        nav: [{
+            Link: '/',
+            Text: 'Home'
+        }]
+    });
 });
 
 app.listen(port, () => console.log(`App is running on ${port}`));
