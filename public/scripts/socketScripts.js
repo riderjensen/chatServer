404088f10a3e949c3f0b9be8bb72fd82760e51e5
Unknown <riderjensen@gmail.com>
2018-04-21 22:44:47 -0600

    A lot of styling on chat and adding JavaScript restrictions on chat plus extra information

--- public/scripts/socketScripts.js
+++ public/scripts/socketScripts.js
@@ -1,37 +1,90 @@
 var socket = io('http://127.0.0.1:3000/');
 // connection to socket IO and sending information to the server
 
+
+// sending information to database
 const chatButton = document.getElementById('chatButton');
 chatButton.onclick = () => {
     const userMessage = document.getElementById('inputField').value;
-    const date = new Date();
-    const timestamp = date.getTime();
-    const username = document.getElementById('username').innerHTML;
-    const userTotal = [username, userMessage, timestamp];
-    socket.emit('message', userTotal);
+    if ((userMessage != '')){
+        const date = new Date();
+        const timestamp = date.getTime();
+        const username = document.getElementById('username').innerHTML;
+        const userTotal = [username, userMessage, timestamp];
+        socket.emit('message', userTotal);
+    }
+    
 };
 
+// scrolling window 
+function scrollWindow() {
+    var elem = document.getElementById('userMessages');
+    elem.scrollTop = elem.scrollHeight;
+  };
+
+// creating text node for incoming chat messages
 function createText(username, className, time, message) {
-    const pTag = document.createElement('p');
-    const textNode = document.createTextNode(`${username} ${time}`);
-    pTag.appendChild(textNode);
+    // image
+    const userImage = document.createElement('img');
+    userImage.classList.add('userImage');
+    userImage.setAttribute('src', '../images/placeholder.png');
+    // username
+    const userTag = document.createElement('p');
+    const userTextNode = document.createTextNode(`${username}`);
+    userTag.appendChild(userTextNode);
+    // div for username and image
+    const usernameAndImageDiv = document.createElement('div');
+    usernameAndImageDiv.appendChild(userImage);
+    usernameAndImageDiv.appendChild(userTag);
+    // time
+    const timeSpan = document.createElement('span');
+    timeSpan.classList.add('userTime');
+    const timeTextNode = document.createTextNode(`${time} `);
+    timeSpan.appendChild(timeTextNode);
+    // message
     const userInfo = document.createElement('p');
-    const userInfoTextNode = document.createTextNode(`${message}`);
-    userInfo.appendChild(userInfoTextNode);
+    const userMessageNode = document.createTextNode(`${message}`);
+    // add
+    userInfo.appendChild(timeSpan);
+    userInfo.appendChild(userMessageNode);
     const divTag = document.createElement('div');
     divTag.classList.add(className);
-    divTag.appendChild(pTag);
+    divTag.appendChild(usernameAndImageDiv);
     divTag.appendChild(userInfo);
     return divTag;
+}
+
+function createTextNoUsername(message, time) {
+    // time
+    const timeSpan = document.createElement('span');
+    timeSpan.classList.add('userTime');
+    const timeTextNode = document.createTextNode(`${time} `);
+    timeSpan.appendChild(timeTextNode);
+    // message
+    const userInfo = document.createElement('p');
+    const userInfoTextNode = document.createTextNode(`${message}`);
+    // add
+    userInfo.appendChild(timeSpan);
+    userInfo.appendChild(userInfoTextNode);
+    document.getElementById('userMessages').lastChild.appendChild(userInfo);
 }
 
 socket.on('returnMessage', (userData) => {
     const currentTime = new Date(userData[2]).toLocaleTimeString();
     if (userData[0] === document.getElementById('username').innerHTML) {
-        document.getElementById('userMessages').appendChild(createText(userData[0], 'sentFromHere', currentTime, userData[1]));
+        if (document.getElementById('userMessages').lastElementChild.classList.contains('sentFromHere') === true) {
+            createTextNoUsername(userData[1], currentTime);
+        } else {
+            document.getElementById('userMessages').appendChild(createText(userData[0], 'sentFromHere', currentTime, userData[1]));
+        }      
     } else {
-        document.getElementById('userMessages').appendChild(createText(userData[0], 'sentFromOther', currentTime, userData[1]));
+        if (document.getElementById('userMessages').lastElementChild.classList.contains('sentFromOther') === true) {
+            createTextNoUsername(userData[1], currentTime);
+        } else {
+            document.getElementById('userMessages').appendChild(createText(userData[0], 'sentFromOther', currentTime, userData[1]));
+        }      
     }
+    scrollWindow();
 });
 
 // Enter pushes message out
@@ -44,3 +97,4 @@ input.addEventListener('keyup', (event) => {
         input.value = '';
     }
 });
+
