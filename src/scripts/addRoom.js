0435a0634dceb1698b90b20067b91c6bc5b7e236
Unknown <riderjensen@gmail.com>
2018-04-26 15:56:51 -0600

    Sign in broken, check comments on addRoom.js

--- src/scripts/addRoom.js
+++ src/scripts/addRoom.js
@@ -20,7 +20,13 @@ module.exports =  {
                 const addRoomHere = await col.findOne({ username });
                 // find owner of the chat room
                 const usernameToAdd = await col.findOne({ _id: new ObjectID(_id) });
-                console.log(addRoomHere.rooms);
+                console.log(addRoomHere.rooms[0]);
+                for(let j=0; j < addRoomHere.rooms.length; j++){
+                    for(let i=0; i < addRoomHere.rooms[j].length; i++){
+                        console.log(addRoomHere.rooms[j][i].Link);
+                        // if this link is the same as username to add's id then dont add
+                    }
+                }
                 const addRoomHereUser = {
                     username:username
                 };
