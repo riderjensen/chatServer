2cd4cee73c93122e98a64d6efd6052d31ca24ba7
Unknown <riderjensen@gmail.com>
2018-04-30 13:53:51 -0600

    Changes to js on other computer

--- src/scripts/extraScripts.js
+++ src/scripts/extraScripts.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const { MongoClient, ObjectID } = require('mongodb');
 
-module.exports =  {
-    addRoom: function(data) {
+module.exports = {
+    addRoom(data) {
         const { _id } = data;
         const { username } = data;
         const url = 'mongodb://localhost:27017';
         const dbName = 'chatServer';
-
         (async function mongo() {
             let client;
             try {
                 client = await MongoClient.connect(url);
-
                 const db = client.db(dbName);
-
                 const col = await db.collection('users');
                 // search for current user of the chat
                 const addRoomHere = await col.findOne({ username });
